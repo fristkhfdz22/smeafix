4cfba4cc972e93cd04a2e681aa6bef3a90a8b7a9
khafidz <152137338+fristkhfdz22@users.noreply.github.com>
2024-10-11 09:03:38 +0700

    Update login.php

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -2,72 +2,125 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8" />
+    <title>Log In | Smk N 1 Slawi</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login</title>
-    <!-- Link CSS Bootstrap -->
-    <link rel="stylesheet" href="<?php echo base_url('template/assets/css/bootstrap.min.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('template/assets/css/styles.css'); ?>"> <!-- Link CSS tambahan jika ada -->
-    <link rel="shortcut icon" href="<?php echo base_url('template/assets/image/smkn 1 slawi.png'); ?>"> <!-- Ganti dengan icon yang sesuai -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet"> <!-- Menghubungkan font Poppins -->
-    <style>
-        body {
-            background-image: url('<?php echo base_url('template/assets/img/back.jpg'); ?>'); /* Ganti dengan nama file gambar Anda */
-            background-size: cover; /* Menutupi seluruh halaman */
-            background-repeat: no-repeat; /* Tidak mengulang gambar */
-            background-position: center; /* Menempatkan gambar di tengah */
-            font-family: 'Poppins', sans-serif; /* Menggunakan font Poppins */
-        }
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-        .form-label {
-            font-weight: 600; /* Membuat label lebih tebal */
-        }
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="<?php echo base_url('template/assets/img/smkn 1 slawi.png'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('template/assets/css/login.css'); ?>">
 
-        .logo {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 20px; /* Jarak antara logo dan judul */
-        }
-    </style>
+    <!-- App css -->
+    <link href="assets/bootstrap/css/config/default/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/bootstrap/css/config/default/app.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="assets/bootstrap/css/config/default/bootstrap-dark.min.css" rel="stylesheet" type="text/css" disabled="disabled" />
+    <link href="assets/bootstrap/css/config/default/app-dark.min.css" rel="stylesheet" type="text/css" disabled="disabled" />
+
+    <!-- icons -->
+    <link href="assets/bootstrap/css/icons.min.css" rel="stylesheet" type="text/css" />
+
 </head>
 
-<body>
-    <div class="container">
-        <div class="row justify-content-center" style="margin-top: 10%;">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="logo">
-                            <img src="<?php echo base_url('template/assets/img/smea.png'); ?>" alt="Logo" style="max-width: 50%; height: auto;">
-                        </div>
-                        <h2 class="text-center">SMEANS | SIGN IN</h2>
-                        <?php if (isset($error)): ?>
-                            <div class="alert alert-danger"><?php echo $error; ?></div>
-                        <?php endif; ?>
-                        <form action="<?php echo site_url('admin/auth'); ?>" method="post">
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Username:</label>
-                                <input type="text" name="username" class="form-control" placeholder="Enter your username" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password:</label>
-                                <input type="password" name="password" class="form-control" placeholder="Enter your password" required>
-                            </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="remember" checked>
-                                <label class="form-check-label" for="remember">Remember me</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-block">Log In</button>
-                        </form>
-                      
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<body class="loading authentication-bg authentication-bg-pattern">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-image: url('<?php echo base_url('template/assets/img/pp smk3.jpg'); ?>');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+    </style>
 
-    <!-- Link JS Bootstrap -->
+
+    <div class="account-pages my-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-4">
+                    <!-- Notification Placeholder -->
+                    <div id="notif"></div>
+
+                    <div class="card mt-3">
+                        <div class="card-body p-4">
+                            <div class="text-center mb-3">
+                                <a href="index.html">
+                                    <img src="<?php echo base_url('template/assets/img/smkn 1 slawi.png'); ?>" alt="Smea Logo" height="90" class="mx-auto">
+                                </a>
+                            </div>
+                            <br>
+                            <div class="text-center mb-3">
+                                <h4 class="text-uppercase mt-0">ADMINISTRATOR | SIGN IN</h4>
+                                <?php if (isset($error)): ?>
+                                    <div class="alert alert-danger"><?php echo $error; ?></div>
+                                <?php endif; ?>
+                            </div>
+
+                            <!-- Login form -->
+                            <form action="<?php echo site_url('admin/auth'); ?>" method="POST">
+                                <div class="mb-3">
+                                    <label for="username" class="form-label">Username</label>
+                                    <br>
+                                    <input name="username" class="form-control" type="text" id="username"
+                                        required="" placeholder="Enter your username">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <br>
+                                    <input name="password" class="form-control" type="password" id="password"
+                                        required="" placeholder="Enter your password">
+                                </div>
+
+                                <div class="mb-3">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
+                                        <label class="form-check-label" for="checkbox-signin">Remember me</label>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="mb-3 d-grid text-center">
+                                    <button class="btn btn-primary" type="submit"> Log In </button>
+                                </div>
+                            </form>
+                            <br>
+
+                            <!-- Register link -->
+                            <p class="text-center">Don't Have an Account?
+                                <!-- <a href="register.html" class="text-muted ms-1"><b>Register</b></a> -->
+                            </p>
+                        </div> <!-- end card-body -->
+                    </div> <!-- end card -->
+
+                    <div class="row mt-3">
+                        <!-- <div class="col-12 text-center">
+                            <p>
+                                <a href="recoverpw.html" class="text-muted ms-1">
+                                    <i class="fa fa-lock me-1"></i> Forgot your password?
+                                </a>
+                            </p>
+                        </div> end col -->
+                    </div> <!-- end row -->
+                </div> <!-- end col -->
+            </div> <!-- end row -->
+        </div> <!-- end container -->
+    </div> <!-- end page -->
+
+    <!-- Vendor js -->
+    <script src="<?php echo base_url('template/assets/js/bootstrap/js/vendor.min.js'); ?>"></script>
+
+    <!-- App js -->
     <script src="<?php echo base_url('template/assets/js/bootstrap.bundle.min.js'); ?>"></script>
+
+
 </body>
 
 </html>
